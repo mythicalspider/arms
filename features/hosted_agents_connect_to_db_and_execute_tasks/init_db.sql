75f3f460d638adfdf0f6c7df15c261373d423cf3
Jeremy Gurr <jeremy.gurr@nextiva.com>
2017-08-10 13:41:47 +0000

    got simple_test_execution test working

--- features/hosted_agents_connect_to_db_and_execute_tasks/init_db.sql
+++ features/hosted_agents_connect_to_db_and_execute_tasks/init_db.sql
@@ -30,11 +30,19 @@ create table if not exists task_request (
   , foreign key (script_id) references script(script_id) on update cascade on delete cascade
 );
 
+create table if not exists task_request_not_done (
+  task_request_id int not null primary key
+  , foreign key (task_request_id) references task_request(task_request_id) on update cascade on delete cascade
+);
+
 create table if not exists task_status_change (
   task_status_change_id int auto_increment not null primary key
   , task_request_id int not null
   , host_id int not null
-  , output_id int comment 'null means the output no longer exists'
+  , output_id int comment 'null means the output no longer exists, or it does not apply to this status'
+  , event_time datetime not null
+  , new_status enum('started', 'succeeded', 'failed') not null
+  , foreign key (task_request_id) references task_request(task_request_id) on update cascade on delete cascade
   , foreign key (host_id) references host(host_id) on update cascade on delete cascade
   , foreign key (output_id) references script(script_id) on update cascade on delete set null
 );
