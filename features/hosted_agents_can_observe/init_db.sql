b67ba80b978252164943dffe39d541692dd251df
Jeremy Gurr <jeremy.gurr@nextiva.com>
2017-08-19 23:05:15 +0000

    m

--- features/hosted_agents_can_observe/init_db.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists observation_status (
-  task_request_recurring_id int auto_increment not null primary key
-  , host_id int not null
-  , wait_until datetime comment 'null means execute immediately'
-  , script_id int not null
-  , parameters varchar(255) not null
-  , foreign key (host_id) references host(host_id) on update cascade on delete cascade
-  , foreign key (script_id) references script(script_id) on update cascade on delete cascade
-);
--- features/property_base/init_db.sql
+++ features/property_base/init_db.sql
@@ -8,8 +8,6 @@ create table if not exists prop (
   prop_id int auto_increment not null primary key
   , name varchar(50) not null
   , prop_class_id int not null
-  , observation_frequency tinyint not null comment '0 means no auto observation. 10 means wait 10 seconds between observations, 20=100 secs, 30=1000 secs'
-  , max_severity int default 0 not null comment '0 means failure will never generate alerts, 1 means minor failure, 2 major, etc'
   , host_id int not null comment 'immutable: should be set at creation time and never changed'
   , active enum('yes', 'no') default 'yes' comment 'set to no when this prop is retired. Record is kept so history is consistent'
   , index (name)
